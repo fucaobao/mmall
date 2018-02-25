cca01ae86e6828c9d9635a17bc79db735c32f4e5
fucaobao <466368084@qq.com>
2018-02-25 22:19:46 +0800

    practice

--- src/app.jsx
+++ src/app.jsx
@@ -3,15 +3,27 @@ import React from 'react';
 // react-dom
 import ReactDom from 'react-dom';
 
-import 'font-awesome/css/font-awesome.min.css'
-import './index.css'
 import './index.scss'
+let name = 'wangjp'
+let names = ['wangjp', 'fuyy', 'liaowy']
+let style = {
+    // color: 'red',
+    // 'fontSize': '18px'
+}
+let jsx = (<div className="jsx" style={style}>
+            {/*条件判断*/}
+            {
+                Math.random() > 0.5 ? <p>I am wangjp</p> : <p>I am not wangjp</p>
+            }
+            {/*数组循环*/}
+            {
+                names.map((name,index) => 
+                    <p key={index}>I am {name}</p>
+                )
+            }
+          </div>)
 
-// render router
 ReactDom.render(
-    <div>
-        <i className="fa fa-address-book"></i>
-        <h1>hello world!</h1>,
-    </div>,
+    jsx,
     document.getElementById('app')
-);
+)
